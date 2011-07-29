664657b33f59972dcd84c49def3e6a8b72e1e639
Charles Chen <ccchen920@gmail.com>
2011-07-28 21:37:21 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RapleafApp::Application.config.secret_token = 'b6c20c7459963efd7b26cd491220e39b76f24689c379ee16a82c19cdd82bf5315b50ab7f7591f4e03aa70dc7c700c542d78622be552e4a6148ff56514caf05bd'
